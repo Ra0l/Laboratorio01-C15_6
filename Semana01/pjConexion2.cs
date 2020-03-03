e4eb418340d5fd141a2603e2863b5234879b523e
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-03-02 19:37:03 -0500

    first commit

---
+++ Semana01/pjConexion2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Semana01
+{
+    public partial class frmClientes : Form
+    {
+        public frmClientes()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection("Data Source=(local);Initial Catalog=Neptuno;Integrated Security=True");
+
+        public void ListaClientes()
+        {
+            using(SqlDataAdapter Df = new SqlDataAdapter("Select * from Clientes", cn))
+            {
+                using (DataSet Da = new DataSet())
+                {
+                    Df.Fill(Da, "Clientes");
+
+                    dgClientes.DataSource = Da.Tables["Clientes"];
+
+                    lblTotal.Text = Da.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'neptunoDataSet1.clientes' Puede moverla o quitarla según sea necesario.
+            this.clientesTableAdapter.Fill(this.neptunoDataSet1.clientes);
+            ListaClientes();
+        }
+    }
+}
---
+++ Semana01/pjConexion3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Semana01
+{
+    public partial class lblCantidad : Form
+    {
+        public lblCantidad()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Leon"].ConnectionString);
+
+        public void ListaProductos()
+        {
+            using(SqlDataAdapter df = new  SqlDataAdapter("Usp_ListaProductos_Neptuno", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using(DataSet Da= new DataSet())
+                {
+                    df.Fill(Da, "Productos");
+                    dgProductos.DataSource = Da.Tables["Productos"];
+                    label3.Text = Da.Tables["Productos"].Rows.Count.ToString();
+                }
+            }
+        }
+
+        private void pjConexion3_Load(object sender, EventArgs e)
+        {
+            ListaProductos();
+        }
+    }
+}
